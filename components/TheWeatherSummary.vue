847c455cad1b80c653ccdbb9a68d7aef18ad9686
Alexander Derevkov <78501617+Xardas225@users.noreply.github.com>
2023-06-05 17:42:24 +0300

    add components, add api key for api, setup getWeather

---
+++ components/TheWeatherSummary.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div class="summary">
+    <div class="pic-main"></div>
+    <div class="weather">
+      <div class="temp">22 °C</div>
+      <div class="weather-desc text-block">Thunderstorm</div>
+    </div>
+    <div class="city text-block">Sochi, FR</div>
+    <div class="date text-block">June 20, 2023</div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import '@/assets/styles/variables'
+@import '@/assets/styles/weatherSummary'
+</style>
---
+++ constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'be4dd084a06aa7be3667f0ae1abc4dee'
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather' 
